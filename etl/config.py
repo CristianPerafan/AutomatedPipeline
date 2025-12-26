2698927f3a0023e1631916e5bd5631e4850569ca
CristianPerafan <cristianperafan67@gmail.com>
2025-12-26 17:38:51 -0500

    Feature: Add extract module to etl (This module uses coingecko free API)

--- etl/config.py
+++ etl/config.py
@@ -8,9 +8,11 @@ load_dotenv()
 logging.basicConfig(level=logging.INFO)
 
 class Config:
-    ASSETS_LIST = str(os.getenv("ASSET_LIST")).split(",")
+    COIN_LIST = str(os.getenv("COIN_LIST")).split(",")
     DAYS_BACK = int(os.getenv("DAYS_BACK", "30"))
+    DATA_DIR = os.getenv("DATA_DIR", "data/")
 
     @classmethod
     def display_config(cls):
-        print(f"Assets to process: {cls.ASSETS_LIST}")
+        print(f"Coins to process: {cls.COIN_LIST}")
+        print(f"Days back: {cls.DAYS_BACK}")
--- etl/extract.py
+++ etl/extract.py
@@ -1,40 +1,98 @@
 import logging
 import requests
 import time
+import json
+
+import polars as pl
+
+from typing import Dict, List
+from pathlib import Path
+
 
 from etl.config import Config
+from utils.http import create_http_session
 
 logger = logging.getLogger("etl.extract")
 
-def fetch_assets():
 
-    assets_list = Config.ASSETS_LIST
-    days_back = Config.DAYS_BACK
+def fetch_coin(
+    session: requests.Session,
+    symbol: str,
+    days_back: int,
+) -> Dict:
+    base_url = "https://api.coingecko.com/api/v3/coins"
+    endpoint = f"{base_url}/{symbol}/market_chart"
+    params = {
+        "vs_currency": "usd",
+        "days": days_back,
+    }
 
-    for asset in assets_list:
-        logger.info(f"Fetching data for asset: {asset} for the past {days_back} days")
-        url = f"https://api.coingecko.com/api/v3/coins/{asset}/market_chart"
-        params = {
-            "vs_currency": "usd",
-            "days": days_back,
-            "interval": "daily"
+    try:
+        response = session.get(endpoint, params=params)
+        response.raise_for_status()
+        data = response.json()
+        logger.info(f"Fetched data for {symbol}")
+        return {
+            "symbol": symbol,
+            "data": data,
         }
+    except requests.RequestException as e:
+        logger.error(f"Error fetching data for {symbol}: {e}")
+        return {}
+    
+def fetch_coins_data(
+    symbols: list,
+    days_back: int,
+    sleep_seconds: int = 2,
+) ->  List[Dict]:
+    if not symbols:
+        raise ValueError("The symbols list is empty.")
+    
+    session = create_http_session()
+    coins_data = []
 
-        print(f"Requesting URL: {url} with params: {params}")
+    for symbol in symbols:
+        try:
+            data = fetch_coin(session, symbol, days_back)
+            if data:
+                coins_data.append(data)
+                logger.info(f"Successfully fetched data for {symbol}")
+        except Exception as e:
+            logger.error(f"Unexpected error fetching data for {symbol}: {e}")
 
-        r = requests.get(url, params=params, timeout=100)
-        if r.status_code == 200:
-            data = r.json()
-            logger.info(f"Successfully fetched data for {asset}")
-        else:
-            logger.error(
-                f"Failed to fetch data for {asset}. Status code: {r.status_code}"
-            )
+        time.sleep(sleep_seconds)
 
-        time.sleep(1)
+    
+            
+        
+    
+
+def extract():
+    logger.info("Starting data extraction process")
+
+    assets_data = fetch_coins_data(
+        symbols=Config.COIN_LIST,
+        days_back=Config.DAYS_BACK,
+    )
+
+    RAW_DATA_PATH = Path(Config.DATA_DIR) / "raw" / "coins_data.json"
+    RAW_DATA_PATH.parent.mkdir(parents=True, exist_ok=True)
+    with open(RAW_DATA_PATH, "w") as f:
+        json.dump(assets_data, f)
+        
+    logger.info(f"Saved raw data to {RAW_DATA_PATH}")
+    logger.info("Data extraction process completed")
+
+
+
+
+
+
+
+    
 
         
 
 
 if __name__ == "__main__":
-    fetch_assets()
+    extract()
